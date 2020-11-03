5504152b7662e2906e188ba8f04658733ec20de3
Sergio <sergioobolevich@gmail.com>
2020-11-03 17:47:28 -0300

    form_for init

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
 
     if @user.save
       redirect_to new_user_path
@@ -13,7 +15,24 @@ class UsersController < ApplicationController
     end
   end
 
-  # def user_params
-  #   params.require(:user).permit(:username, :email, :password)
-  # end
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to new_user_path, notice: 'User was successfully updated.' }
+      else
+        redirect_to new_user_path
+      end
+    end
+    redirect_to new_user_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
