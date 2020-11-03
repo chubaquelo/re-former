0dd5664d2fcac5149f699847b6a323855b2be8af
Sergio <sergioobolevich@gmail.com>
2020-11-03 14:51:53 -0300

    Add user model, form view and routes

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  validates :username, presence: true, length: { minimum: 5, maximum: 20 }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Email invalid"  }
+  validates :password, presence: true
+end
